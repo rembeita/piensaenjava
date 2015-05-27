fa8017d86b90d9148519d6948c7ac6c02af04da5
Rodrigo <rembeita@gmail.com>
2015-05-26 23:12:24 -0300

    se incorporaron ejercicios

---
+++ Ejercicios2/src/punto4.java
@@ -0,0 +1,13 @@
+
+public class punto4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		for (i = 0; i < 101; i++)
+		{
+			System.out.println("Valores: " + i);
+		}
+	}
+
+}
---
+++ Ejercicios2/src/punto5.java
@@ -0,0 +1,22 @@
+
+public class punto5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 0;
+		for (i = 0; i < 101; i++)
+		{
+			if ( i == 47 )
+			{
+				break;
+			}
+			System.out.println("Valores: " + i);
+			
+		}
+		
+		
+		
+	}
+
+}
