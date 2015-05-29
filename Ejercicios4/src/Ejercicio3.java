21d1e7e77259dbf1d9cdc8c58e4d75a9961124be
Rodrigo <rembeita@gmail.com>
2015-05-28 22:44:01 -0300

    nuevos ejercicios

---
+++ Ejercicios4/src/Ejercicio3.java
@@ -0,0 +1,30 @@
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean verdadero = true;
+		
+		while (verdadero = true)
+		{
+			for (int i = 0; i < 26; i++){
+				double j =  Math.random();
+				double k =  Math.random();
+			
+				if (j <= k )
+				{
+					System.out.println("J: " + j + "es menor igual a K: " + k );	
+				}
+				else
+				{
+					System.out.println("J: " + j + "es mayor a K: " + k );
+				}
+			
+
+			}
+		
+		}
+		
+	}
+
+}
