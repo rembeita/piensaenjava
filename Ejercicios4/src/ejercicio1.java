6bcb6faae59863ab4852dfc216b3c52dc138a037
Rodrigo <rembeita@gmail.com>
2015-05-28 22:23:30 -0300

    nuevos ejercicios

---
+++ Ejercicios4/src/ejercicio1.java
@@ -0,0 +1,16 @@
+
+public class ejercicio1 {
+	
+	static public void main (String args[]){
+		for (int i = 0; i < 101; i++)
+		{
+
+			System.out.println(i);
+
+			
+			
+		};
+		
+		
+	}
+}
---
+++ Ejercicios4/src/ejercicio2.java
@@ -0,0 +1,26 @@
+
+public class ejercicio2 {
+
+	static public void main (String args[]){
+		
+		for (int i = 0; i < 26; i++){
+			double j =  Math.random();
+			double k =  Math.random();
+			
+			if (j <= k )
+			{
+				System.out.println("J: " + j + "es menor igual a K: " + k );	
+			}
+			else
+			{
+				System.out.println("J: " + j + "es mayor a K: " + k );
+			}
+			
+
+		}
+		
+			
+	}
+	
+	
+}
